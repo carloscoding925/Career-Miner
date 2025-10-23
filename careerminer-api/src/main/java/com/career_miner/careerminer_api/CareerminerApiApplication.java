1d5ae8199f36d05e6d36f075082c037e574d952d
Carlos Hernandez <carlosandresh925@gmail.com>
2025-10-22 23:35:46 -0700

    Init careerminer API

---
+++ careerminer-api/src/main/java/com/career_miner/careerminer_api/CareerminerApiApplication.java
@@ -0,0 +1,13 @@
+package com.career_miner.careerminer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerminerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerminerApiApplication.class, args);
+	}
+
+}
---
+++ careerminer-api/src/test/java/com/career_miner/careerminer_api/CareerminerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.career_miner.careerminer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerminerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
