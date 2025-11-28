b8621a4b7b8d1ffe3716172fd66f5a8a1ede0cf9
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-27 23:49:27 -0800

    Init scraper 10 main try/catch

--- playwright_scrapers/scrapers/0010-citadel-careers-scraper.ts
+++ playwright_scrapers/scrapers/0010-citadel-careers-scraper.ts
@@ -2,7 +2,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { getFilePrefix } from "../utils/naming-util.js";
 import { BandwidthTracker } from "../utils/bandwidth-util.js";
-import { Browser, chromium } from "playwright";
+import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { usageOutputDirectory } from "../constants/directories.js";
+import { deleteOldFiles, writeNewFile } from "../utils/file-io-util.js";
 
 async function scrapeCitadelCareers() {
     console.log("Running Scraper 0010 - Citadel Careers");
@@ -19,6 +21,32 @@ async function scrapeCitadelCareers() {
     const browser: Browser = await chromium.launch({
         headless: process.env.HEADLESS === 'true'
     });
+
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await browser.newPage();
+
+    page.on('requestfinished', async(request) => {
+        await bandwidthTracker.trackRequest(request);
+    });
+
+    try {
+        // Page Navigation
+        console.log("Navigating to Careers Page");
+
+    } catch (error) {
+        console.log("Error Occured While Scraping: " + error);
+    } finally {
+        bandwidthTracker.printSummary();
+
+        /*
+        const outputDir: string = path.join(__dirname, usageOutputDirectory);
+        deleteOldFiles(outputDir, scraperPrefix);
+        writeNewFile(outputDir, scraperPrefix, bandwidthTracker.returnStats());
+        */
+
+        await browser.close();
+        console.log("\n Finished Running - Scraper 0010 - Citadel Careers");
+    }
 }
 
 scrapeCitadelCareers();
