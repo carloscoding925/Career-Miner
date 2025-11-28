33f0cdda5b3ace14a9c9969a19d3843065497885
Carlos Hernandez <carlosandresh925@gmail.com>
2025-11-28 14:10:55 -0800

    Set up initial try-catch block for scraper 0009

--- playwright_scrapers/scrapers/0009-janestreet-careers-scraper.ts
+++ playwright_scrapers/scrapers/0009-janestreet-careers-scraper.ts
@@ -1,3 +1,59 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { getFilePrefix } from "../utils/naming-util.js";
+import { BandwidthTracker } from "../utils/bandwidth-util.js";
+import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { usageOutputDirectory } from "../constants/directories.js";
+import { deleteOldFiles, writeNewFile } from "../utils/file-io-util";
+import { CompanyUrls } from "../models/companies.js";
+
 async function scrapeJaneStreetCareers() {
-    
+    console.log("Running Scraper 0009 - Jane Street Careers");
+
+    // File Variables
+    const __filename: string = fileURLToPath(import.meta.url);
+    const __dirname: string = path.dirname(__filename);
+    const scraperPrefix: string = getFilePrefix(__filename);
+
+    // Init bandwidth tracking util
+    const bandwidthTracker: BandwidthTracker = new BandwidthTracker();
+
+    // Launch Browser
+    const browser: Browser = await chromium.launch({
+        headless: process.env.HEADLESS === 'true'
+    });
+
+    // Browser and Scraper constants
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await context.newPage();
+
+    page.on('requestfinished', async (request) => {
+        await bandwidthTracker.trackRequest(request);
+    });
+
+    try {
+        // Page Navigation
+        console.log("Navigating to Careers Page");
+        await page.goto(CompanyUrls.JANE_STREET, {
+            waitUntil: 'load'
+        });
+
+        await page.waitForTimeout(3000);
+        
+    } catch (error) {
+        console.log("Error Occured While Scraping: " + error);
+    } finally {
+        bandwidthTracker.printSummary();
+
+        /*
+        const outputDir: string = path.join(__dirname, usageOutputDirectory);
+        deleteOldFiles(outputDir, scraperPrefix);
+        writeNewFile(outputDir, scraperPrefix, bandwidthTracker.returnStats());
+        */
+
+        await browser.close();
+        console.log("\n Finished Running - Scraper 0009 - Jane Street Careers");
+    }
 }
+
+scrapeJaneStreetCareers();
