0a0dc804a72a69fac8c85c178d2d719456ac4900
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-04 20:15:27 -0800

    Gather posting title and URL

--- playwright_scrapers/constants/search-terms.ts
+++ playwright_scrapers/constants/search-terms.ts
@@ -3,3 +3,4 @@ export const SEARCH_ENGINEERING: string = 'Engineering';
 export const SEARCH_QUANTITATIVE_RESEARCH: string = 'Quantitative-Research';
 export const SEARCH_TECHNOLOGY: string = 'Technology';
 export const SEARCH_JANE_STREET: string = 'Trading, Research, and Machine Learning';
+export const SEARCH_SOFTWARE: string = 'Software';
--- playwright_scrapers/scrapers/0008-twitch-careers-scraper.ts
+++ playwright_scrapers/scrapers/0008-twitch-careers-scraper.ts
@@ -2,10 +2,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { getFilePrefix } from "../utils/naming-util.js";
 import { BandwidthTracker } from "../utils/bandwidth-util.js";
-import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { Browser, BrowserContext, chromium, Locator, Page } from "playwright";
 import { usageOutputDirectory } from "../constants/directories.js";
 import { deleteOldFiles, writeNewFile } from "../utils/file-io-util";
 import { CompanyUrls } from "../models/companies.js";
+import { SEARCH_SOFTWARE } from "../constants/search-terms.js";
+import { PostingCoverData } from "../models/data-storage.js";
 
 async function scrapeTwitchCareers() {
     console.log("Running Scraper 0008 - Twitch Careers");
@@ -41,6 +43,27 @@ async function scrapeTwitchCareers() {
 
         // Extract Job Title and Posting URLS
         console.log("Extracting Posting URL's");
+        await page.getByRole('link', { name: 'Job Openings' }).click();
+        await page.waitForTimeout(2000);
+
+        const searchBox: Locator = page.getByRole('textbox');
+        await searchBox.waitFor({ state: 'visible' });
+        await searchBox.fill(SEARCH_SOFTWARE);
+        await page.waitForTimeout(2000);
+
+        await page.waitForSelector('.mb-14 .border-b a.text-twitch-purple');
+        const jobs: PostingCoverData[] = await page.locator('.mb-14 .border-b a.text-twitch-purple').evaluateAll((links) => {
+            return links.map(link => ({
+                title: link.textContent?.trim() || '',
+                jobUrl: link.getAttribute('href') || ''
+            }));
+        });
+
+        const jobUrls: PostingCoverData[] = jobs.map(job => ({
+            title: job.title,
+            jobUrl: CompanyUrls.TWITCH + job.jobUrl
+        }));
+        console.log(`Found ${jobUrls.length} Job Postings`);
     } catch (error) {
         console.log("Error Occured While Scraping: " + error);
     } finally {
