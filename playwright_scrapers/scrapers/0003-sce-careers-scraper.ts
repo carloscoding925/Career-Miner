71b069d10fe2bfc9cfff64a40993ab7338799e71
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-20 21:46:48 -0800

    setting up scraper 0003

--- playwright_scrapers/scrapers/0003-sce-careers-scraper.ts
+++ playwright_scrapers/scrapers/0003-sce-careers-scraper.ts
@@ -1,3 +1,41 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { getFilePrefix } from "../utils/naming-util.js";
+import { BandwidthTracker } from "../utils/bandwidth-util.js";
+import { Browser, BrowserContext, chromium, Page } from "playwright";
+
 async function scrapeSouthernEdisonCareers() {
-    
+    console.log("Running Scraper 0003 - Southern California Edison Careers");
+
+    // File Variables
+    const __filename: string = fileURLToPath(import.meta.url);
+    const __dirname: string = path.dirname(__filename);
+    const scraperPrefix: string = getFilePrefix(__filename);
+
+    // Init Bandwidth Tracker
+    const bandwidthTracker: BandwidthTracker = new BandwidthTracker();
+
+    // Launch Browser
+    const browser: Browser = await chromium.launch({
+        headless: process.env.HEADLESS === "true"
+    });
+
+    // Browser and Scraper Constants
+    const context: BrowserContext = await browser.newContext();
+    const page: Page = await browser.newPage();
+
+    // Resource Blocking and Data Usage
+    page.on('requestfinished', async (request) => {
+        await bandwidthTracker.trackRequest(request);
+    });
+
+    try {
+
+    } catch (error) {
+        console.log("Error Occured While Scraping: " + error);
+    } finally {
+
+    }
 }
+
+scrapeSouthernEdisonCareers();
