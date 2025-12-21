7169c650a7c5ab53c58cbac026fffc990ec278c0
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-20 19:10:40 -0800

    apply filter

--- playwright_scrapers/scrapers/0002-pge-careers-scraper.ts
+++ playwright_scrapers/scrapers/0002-pge-careers-scraper.ts
@@ -4,6 +4,7 @@ import { getFilePrefix } from "../utils/naming-util.js";
 import { BandwidthTracker } from "../utils/bandwidth-util.js";
 import { Browser, BrowserContext, chromium, Page } from "playwright";
 import { CompanyUrls } from "../models/companies.js";
+import { SEARCH_INFORMATION_TECHNOLOGY } from "../constants/search-terms.js";
 
 async function scrapePgeCareers() {
     console.log("Running Scraper 0002 - PG&E Careers");
@@ -36,8 +37,32 @@ async function scrapePgeCareers() {
         await page.goto(CompanyUrls.PGE, {
             waitUntil: 'load'
         });
-
         await page.waitForTimeout(1000);
+
+        // Apply Filters
+        console.log("Applying Filters");
+        await page.evaluate(() => {
+            const toggleButton: HTMLButtonElement = document.querySelector('#category-toggle') as HTMLButtonElement;
+            if (toggleButton) {
+                toggleButton.classList.add('expandable-child-open');
+                toggleButton.setAttribute('aria-expanded', 'true');
+            }
+
+            const filterList: HTMLElement = document.querySelector('#category-toggle + .search-filter-list') as HTMLElement;
+            if (filterList) {
+                filterList.classList.add('expandable-childlist-open');
+            }
+
+            const checkbox: HTMLInputElement = document.querySelector('input[data-facet-type="1"][data-display="Information Technology"]') as HTMLInputElement;
+            if (checkbox) {
+                checkbox.checked = true;
+                checkbox.dispatchEvent(new Event('change', { bubbles: true }));
+                checkbox.dispatchEvent(new Event('click', { bubbles: true }));
+            }
+        });
+        await page.waitForTimeout(2000);
+
+        
     } catch (error) {
         console.log("Error Occured While Scraping: " + error);
     } finally {
