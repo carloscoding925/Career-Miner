db3302f8764f934aa7c662e3f97f1960886ea2ef
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-24 12:53:13 -0800

    Gather job titles and URLS

--- playwright_scrapers/scrapers/0004-itslogistics-careers-scraper.ts
+++ playwright_scrapers/scrapers/0004-itslogistics-careers-scraper.ts
@@ -2,9 +2,10 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { getFilePrefix } from "../utils/naming-util.js";
 import { BandwidthTracker } from "../utils/bandwidth-util.js";
-import { Browser, BrowserContext, chromium, Page } from "playwright";
+import { Browser, BrowserContext, chromium, Locator, Page } from "playwright";
 import { CompanyUrls } from "../models/companies.js";
 import { SEARCH_TECH } from "../constants/search-terms.js";
+import { PostingCoverData } from "../models/data-storage.js";
 
 async function scrapeCreditOneCareers() {
     console.log("Running Scraper 0004 - ITS Logistics Careers");
@@ -39,11 +40,28 @@ async function scrapeCreditOneCareers() {
 
         await page.waitForTimeout(3000);
 
+        // Filter jobs by Department
         console.log(`Filtering Jobs by Department: ${SEARCH_TECH}`);
         await page.locator('#react-select-department-type-filter-input').click();
         await page.getByRole('option', { name: SEARCH_TECH }).click();
 
-        await page.waitForTimeout(3000);
+        // Extract Job URLS, titles, and days since posted
+        const jobWrappers: Locator[] = await page.locator('.Job-module-scss-module__MPRUTW__wrapper').all();
+        const jobUrls: PostingCoverData[] = [];
+        const postedDates: string[] = [];
+
+        for (let i = 0; i < jobWrappers.length; i++) {
+            const title: string = await jobWrappers[i].locator('.Job-module-scss-module__MPRUTW__name').textContent() ?? "";
+            const link: string = await jobWrappers[i].locator('a.Button-module-scss-module__Ib45na__button').getAttribute('href') ?? "";
+            const postedDays = await jobWrappers[i].locator('.Job-module-scss-module__MPRUTW__postedDays').textContent() ?? "";
+
+            jobUrls.push({
+                title: title.trim(),
+                jobUrl: link.trim()
+            });
+
+            postedDates.push(postedDays.trim());
+        }
     } catch (error) {
         console.log("Error Occured While Scraping: " + error);
     } finally {
