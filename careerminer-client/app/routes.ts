95b8f05bdd6f20ba023d40dd330459b1eb26f978
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-26 15:22:30 -0800

    add flat routes

--- careerminer-client/app/routes.ts
+++ careerminer-client/app/routes.ts
@@ -1,3 +1,11 @@
+import { remixRoutesOptionAdapter } from "@react-router/remix-routes-option-adapter";
+import { flatRoutes } from "remix-flat-routes";
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default remixRoutesOptionAdapter((defineRoutes) => {
+    return flatRoutes("routes", defineRoutes, {
+        ignoredRouteFiles: [
+            '**/.*'
+        ],
+    });
+}) satisfies RouteConfig;
