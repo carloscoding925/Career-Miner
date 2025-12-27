083ac4c43807c29e82bd55b122e2d99abe679b68
Carlos Hernandez <carlosandresh925@gmail.com>
2025-12-27 14:04:39 -0800

    Create database util

---
+++ careerminer-api/README.md
@@ -0,0 +1,3 @@
+# To Set Database Connection
+
+export JDBC_URI="jdbc:postgresql://localhost:5432/postgres?user=USERNAME&password=PASSWORD"
--- careerminer-api/src/main/java/com/career_miner/careerminer_api/utils/DatabaseUtil.java
+++ careerminer-api/src/main/java/com/career_miner/careerminer_api/utils/DatabaseUtil.java
@@ -4,12 +4,45 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 public class DatabaseUtil {
     private static final Logger logger = LogManager.getLogger(DatabaseUtil.class);
+    private static DatabaseUtil instance;
+    private HikariDataSource dataSource;
     
     private DatabaseUtil() {
+        logger.info("Creating Hikari Config");
+
         HikariConfig config = new HikariConfig();
-        logger.info("Initializing Database");
+        config.setJdbcUrl(System.getenv("JDBC_URI"));
+
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(1);
+
+        config.setConnectionTimeout(30000);
+        config.setIdleTimeout(600000);
+        config.setMaxLifetime(1800000);
+
+        config.setPoolName("CareerMiner Pool");
+        config.setConnectionTestQuery("SELECT 1");
+        config.setLeakDetectionThreshold(60000);
+
+        config.setAutoCommit(true);
+        config.setKeepaliveTime(60000);
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public static synchronized DatabaseUtil getInstance() {
+        if (instance == null) {
+            instance = new DatabaseUtil();
+        }
+
+        return instance;
+    }
+
+    public HikariDataSource getDataSource() {
+        return dataSource;
     }
 }
